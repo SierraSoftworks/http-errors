3ac965f1b0d281df15681d690be104e03b334c32
Benjamin Pannell <benjamin@pannell.dev>
2024-05-23 22:33:32 +0100

    feat: Add a healthcheck endpoint

--- src/server.rs
+++ src/server.rs
@@ -46,6 +46,22 @@ impl<B> Service<Request<B>> for ErrorService {
     type Future = Ready<Result<Response<Full<Bytes>>, Infallible>>;
 
     fn call(&self, req: Request<B>) -> Self::Future {
+        match req.uri().path() {
+            "/.well-known/health" => {
+                return ready(Ok(Response::builder()
+                    .status(StatusCode::OK)
+                    .body(Full::new(Bytes::new()))
+                    .unwrap()))
+            }
+            "/favicon.ico" => {
+                return ready(Ok(Response::builder()
+                    .status(StatusCode::NOT_FOUND)
+                    .body(Full::new(Bytes::new()))
+                    .unwrap()))
+            }
+            _ => {}
+        }
+
         if let Some(response) = self.cache.get(req.uri().path()) {
             return ready(Ok(response.clone()));
         } else if let Some(response) = self.cache.get("/504") {
@@ -61,7 +77,40 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn test_handle() {
+    async fn test_handle_health_check() {
+        let service = ErrorService::new(StatusCode::NOT_FOUND);
+        let req = Request::builder()
+            .uri("/.well-known/health")
+            .body(Full::new(Bytes::new()))
+            .unwrap();
+        let response = service.call(req).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_handle_favicon() {
+        let service = ErrorService::new(StatusCode::NOT_FOUND);
+        let req = Request::builder()
+            .uri("/favicon.ico")
+            .body(Full::new(Bytes::new()))
+            .unwrap();
+        let response = service.call(req).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[tokio::test]
+    async fn test_handle_default_error() {
+        let service = ErrorService::new(StatusCode::NOT_FOUND);
+        let req = Request::builder()
+            .uri("/unknown")
+            .body(Full::new(Bytes::new()))
+            .unwrap();
+        let response = service.call(req).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[tokio::test]
+    async fn test_handle_known_errors() {
         let service = ErrorService::new(StatusCode::NOT_FOUND);
 
         for error in ERRORS.iter() {
